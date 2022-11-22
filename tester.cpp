6dc889741bfd81d9341119d22b8fd2afa23504ee
Ayush Chaudhary <ayushchaudhary360@gmail.com>
2022-11-22 21:48:33 +0530

    Create tester.cpp

---
+++ tester.cpp
@@ -0,0 +1,33 @@
+// 279. Perfect Squares
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    //driver
+    return 0;
+}
+class Solution 
+{
+public:
+    int numSquares(int n) 
+    {
+        if (n <= 0)
+        {
+            return 0;
+        }
+        static vector<int> cntPerfectSquares({0});
+        while (cntPerfectSquares.size() <= n)
+        {
+            int m = cntPerfectSquares.size();
+            int cntSquares = INT_MAX;
+            for (int i = 1; i*i <= m; i++)
+            {
+                cntSquares = min(cntSquares, cntPerfectSquares[m - i*i] + 1);
+            }
+            
+            cntPerfectSquares.push_back(cntSquares);
+        }
+        
+        return cntPerfectSquares[n];
+    }
+};
